234f360300ccd3cf52f921ee76974f963c515976
iamramyak <iamramyak7@gmail.com>
2024-03-28 15:41:50 +0530

    springboot-kafka

---
+++ src/test/java/kafka/springboot/kafka/project/SpringbootKafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kafka.springboot.kafka.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
